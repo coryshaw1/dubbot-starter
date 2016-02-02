8af74ab78c203916fc24fb9d3d18a2adad6f2887
nitroghost <wildeman.benjamin@gmail.com>
2016-02-02 05:11:54 +0000

    forgot to actually add the sexy time formatting. such a dodo bird

--- app/bot/index.js
+++ app/bot/index.js
@@ -9,7 +9,7 @@ var DubAPI = require("dubapi"),
 //yes I know I could have declared them once here then pass it to bot but
 //I didn't want to. leaf me alone :'(
 log.setUTC(true);
-
+log.setTimeformat("YYYY-MM-DD HH:mm:ss:SSS");
 new DubAPI({
     username: config.botName,
     password: config.botPass
@@ -22,7 +22,7 @@ new DubAPI({
     //to find out how to use jethro visit: https://github.com/JethroLogger/Jethro
     bot.log = require("jethro");
     //get a useable date/time format. the default sucks!
-    bot.log.setTimeformat();
+    bot.log.setTimeformat("YYYY-MM-DD HH:mm:ss:SSS");
     //you may want to remove this setting. I had it because my bot is hosted in
     //the states, and don't know what timezone, so for my logs I know exactly
     //what time things happened. yay for knowing stuff!
