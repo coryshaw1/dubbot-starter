4fad643786ef263138efe4a575dbfe9cd0902f80
Nitro Ghost <wildeman.benjamin@gmail.com>
2016-02-03 21:22:42 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	//cannot leave a file blank because git. so have my evironment variables :D
 	ipaddress: process.env.OPENSHIFT_NODEJS_IP,
-	port: process.env.OPENSHIFT_NODEJS_IP,
+	port: process.env.OPENSHIFT_NODEJS_PORT,
 	botName: process.env.BOT_NAME,
 	botPass: process.env.BOT_PASS,
 	roomURL: process.env.ROOM_URL
